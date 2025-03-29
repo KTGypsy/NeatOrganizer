848e6ed47d23b0c23a2b869835f2beef1f211d35
Rafal Kapela <84186160+ktgypsy@users.noreply.github.com>
2025-03-29 09:59:49 +0100

    Reorganize dependencies and add locally ones that are no lnger available

---
+++ materialintro/src/main/java/com/codertainment/materialintro/shape/Focus.kt
@@ -0,0 +1,11 @@
+package com.codertainment.materialintro.shape
+
+/**
+ * Focus on target with circle
+ * These enum decides circle radius.
+ */
+enum class Focus {
+
+  MINIMUM,
+  NORMAL, ALL
+}
---
+++ materialintro/src/main/java/com/codertainment/materialintro/shape/ShapeType.kt
@@ -0,0 +1,10 @@
+package com.codertainment.materialintro.shape
+
+/**
+ * Allows the target area to be highlighted by either a circle or rectangle
+ */
+enum class ShapeType {
+
+  CIRCLE,
+  RECTANGLE
+}
